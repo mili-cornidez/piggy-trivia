e716a559c7a2807bdcc6b69934796a3a86cbc37f
mili-cornidez <milagroscornidez@gmail.com>
2024-12-15 00:57:03 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/tokens", tokensRoutes);
 app.use("/paymaster", paymasterRoutes);
 //app.use("/transaction", transactionRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
